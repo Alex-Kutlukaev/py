eebe9d5152bc872407bc90365f5ce2a40fa28c14
Алексей Кутлукаев <alekseykutlukaev@192.168.1.5>
2022-10-18 14:30:55 +0300

    add zodiac

---
+++ знак зодиака.py
@@ -0,0 +1,18 @@
+month = input("Введите месяц рождения: ")
+day = int(input("Введите день рождения: "))
+if (month == 'январь') and day <= 21:
+    print('козерог')
+elif month == ('январь'):
+     print('водолей')
+elif(month == 'февраль') and day <=21:
+    print('водолей')
+elif month == ('февраля'):
+    print('рыба')
+elif(month == 'март') and day < 21:
+    print('рыба')
+elif month == ('март'):
+    print('овен')
+elif(month == 'апрель') and day < 20:
+    print('овен')
+elif month == ('апрель'):
+    print('телец')
